f75b06e00e9705e19f5ff82d9fa4df2f88010c72
Gargi Chaudhary <gargichaudhary06@gmail.com>
2024-11-08 18:03:12 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// In your routes/web.php or routes/api.php
+Route::get('/check-db-connection', function () {
+    return DB::connection()->getPDO()->getAttribute(PDO::ATTR_CONNECTION_STATUS);
+});
