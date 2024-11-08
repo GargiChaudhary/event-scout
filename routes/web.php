d7b57fc03497a65a0050d1121a710400249f11d1
Gargi Chaudhary <gargichaudhary06@gmail.com>
2024-11-08 19:51:02 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// In your routes/web.php or routes/api.php
-Route::get('/check-db-connection', function () {
-    return DB::connection()->getPDO()->getAttribute(PDO::ATTR_CONNECTION_STATUS);
-});
+
